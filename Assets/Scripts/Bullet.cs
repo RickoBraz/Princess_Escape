4e985956953ea0ed3ed8e22f14e9533df10ff229
RickoBraz <marco.henrique.s.s@gmail.com>
2016-09-23 23:14:49 -0300

    Criar Repositório, Movimentação e Ataque
    Ataque falta polimento

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour 
+{
+    public static Bullet instance;
+    [SerializeField] private GameObject bullet, player;
+    public int balas=5;
+	private Vector2 origem;
+	public KeyCode disparo;
+    private Rigidbody2D player_rb;
+
+    // Update is called once per frame
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Update () {
+		if (Input.GetKeyUp (disparo) && balas > 0) 
+		{
+			origem = new Vector2 (player.transform.position.x, player.transform.position.y  - 2f);
+			Instantiate (bullet, origem, Quaternion.identity);
+            Movimento.instance.RecuoBala();
+            balas -= 1;
+        }
+	}
+
+    public void Recarregar(){balas = 5;}
+}
---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento : MonoBehaviour
+{
+    public static Movimento instance;
+
+    [SerializeField]
+    private KeyCode right, left;
+    [SerializeField]
+    private float velright, velleft, veldown = -15f;
+    private float o_velright, o_velleft;
+    [SerializeField]
+    private int count, time = 10;
+    [SerializeField]
+    private GameObject player;
+    private Rigidbody2D player_rb;
+
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        o_velright = velright;
+        count = 0;
+        StartCoroutine(leftdown());
+        StartCoroutine(getRB());
+        o_velleft = velleft;
+
+    }
+
+    IEnumerator leftdown()
+    {
+        velleft = velleft * -1;
+        yield return new WaitForSeconds(0);
+        print("valores corrigidos");
+    }
+
+    IEnumerator getRB()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        player_rb = player.GetComponent<Rigidbody2D>();
+        yield return new WaitForSeconds(3);
+        print("player is connected");
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(right))
+        {
+            count = 1;
+        }
+        else if (Input.GetKey(left))
+        {
+            count = 2;
+        }
+        else
+        {
+            if (time == 0)
+            {
+                count = 0;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if (count == 1)
+        {
+            if (time > 0) time -= 1;
+            else veldown = -15;
+            player_rb.velocity = new Vector2(velright, veldown);
+        }
+        else if (count == 2)
+        {
+            if (time > 0) time -= 1;
+            else veldown = -15;
+            player_rb.velocity = new Vector2(velleft, veldown);
+        }
+        else if (count == 3)
+        {
+            if (time > 0)
+            {
+                time -= 1;
+                player_rb.velocity = new Vector2(player_rb.velocity.x, veldown = 15);
+            }
+        }
+        else
+        {
+            player_rb.velocity = new Vector2(0f, veldown = -15);
+        }
+    }
+    public void RecuoBala()
+    {
+        time = 10;
+        count = 3;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Plataforma")
+        {
+
+            Bullet.instance.Recarregar();
+        }
+    }
+}
